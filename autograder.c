4d81bad553204e118964f010dc27c5c1795a0de4
Lev632 <114541520+Lev632@users.noreply.github.com>
2024-02-03 15:45:51 -0600

    Add files via upload

--- autograder.c
+++ autograder.c
@@ -3,29 +3,40 @@
 
 int main(int argc, char *argv[]) {
     
-    write_filepath_to_submissions("/home/erist003/4061/p1/test", "/home/erist003/4061/p1/submission.txt");
+    write_filepath_to_submissions("/home/lexxx668/4061/p1/test", "/home/lexxx668/4061/p1/submission.txt");
     FILE *file = fopen("submission.txt", "r");
     if(!file){
         return -1;
     }
     int index = 0;
     pid_t pids[argc];
-    struct timeval timer[argc];
+    // struct timeval timer[argc];
     char buff[128];
+    int scores[argc];
 
     while(fgets(buff, sizeof(buff), file) != NULL){
         pid_t pid = fork();
+        //parent process
         if(pid > 0){
-            pid_t terminated_pid = wait(NULL);
-        }
-        else if(pid == 0){
-            pids[index] =  getpid();
-            start_timer(&timer[index]);
-            if(execl('/home/erist003/4061/p1/submission.txt', buff)==-1){
+            index++;
+            // pid_t terminated_pid = wait(NULL);
+            int status;
+            waitpid(-1, &status, WNOHANG);
 
+            // if the child process completed return the score and store it 
+            if (WIFEXITED(status)){
+                scores[index] = WEXITSTATUS(status);
+                printf("score is: %d \n", scores[index]);
+                printf("pid id: %d \n", pids[index]);
             }
-            
-            stop_timer(&timer[index]);
+        }
+        //child process
+        else if(pid == 0){
+            //get the p id of the child and store it in the array and start the timer for the process
+            pids[index] = getpid();
+            // start_timer(&timer[index]);
+            execl("/home/lexxx668/4061/p1/submission.txt", argv[index],(char *)NULL);
+            // stop_timer(&timer[index]);
         }
     }
 }     
