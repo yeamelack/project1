c1d5a85a0bb527406cc37de5ea8994a246789fea
yeamelack <150837167+yeamelack@users.noreply.github.com>
2024-02-05 17:35:21 -0600

    Update autograder.c

--- autograder.c
+++ autograder.c
@@ -1,6 +1,7 @@
 #include "include/utility.h"
 #include <stdio.h>
 
+
 int main(int argc, char *argv[]) {
     
     write_filepath_to_submissions("/home/erist003/4061/p1/test", "/home/erist003/4061/p1/submission.txt");
@@ -15,6 +16,7 @@ int main(int argc, char *argv[]) {
 
     while(fgets(buff, sizeof(buff), file) != NULL){
         buff[strcspn(buff, "\n")] = 0;
+       
         pid_t pid = fork();
 
         if(pid > 0){
@@ -24,15 +26,32 @@ int main(int argc, char *argv[]) {
             pids[index] =  getpid();
             start_timer(&timer[index]);
 
-            int ans = execl(buff, buff, NULL);
+            for(int i = 0; i < argc; i++){
+                char string[20];
+                sprintf(string, "%d", i);
+                memset(string, 0, sizeof(string));
+                int ans = execl(buff, buff, string, NULL);
+
+
             if(ans == -1){
                 perror("execl failed, try again");
                 stop_timer(&timer[index]);
             }
+
+
+            }
+            
+            
+            // if(ans == -1){
+            //     perror("execl failed, try again");
+            //     stop_timer(&timer[index]);
+            // }
            
             
             //stop_timer(&timer[index]);
         }
     }
+    fclose(file);
+    return 0;
 }     
 
