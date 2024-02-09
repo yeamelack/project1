c7af1bf3f332fb4afa4323d38bae39102539370d
yeamelack <150837167+yeamelack@users.noreply.github.com>
2024-02-09 12:37:40 -0600

    Update WIPautograder.c

--- WIPautograder.c
+++ WIPautograder.c
@@ -11,66 +11,71 @@ int main(int argc, char *argv[]) {
       if(!file){
         return -1;
     }
-    int i = 0;
-    int index = 2;
+
     pid_t pids[argc];
     struct timeval timer[argc];
     char buff[128];
     int numStudents = atoi(argv[1]);
     char students[numStudents][WORD_LEN];
+    int finishedpid = 0;
+    int count = 0;
+    int index = 0;
+    
+    
 
-    
-    while(fgets(buff, sizeof(buff), file) != NULL){
-        buff[strcspn(buff, "\n")] = 0;
-        strcpy(students[i], buff);
-        i++;
-    }
-    
-    for (int k = 2; k < argc; k++){
-        for (int j = 0; j < numStudents; j++){
+    for(int i=0; i < 3; i++){
+        for(int j=0; j<atoi(argv[1]); j++){
             pid_t pid = fork();
-            if(pid == 0){
-                pids[index] = getpid();
-                execl(students[j], argv[1], argv[k], NULL); 
-                index++ 
-
+            if(pid>0){
+                    pid_t terminated_pid = wait(NULL);
+                }
+                else if(pid == 0){
+                    pids[j] = getpid();
+                    printf("%s%d", "\n", pids[j]);
+                    execl(students[i], argv[1], argv[j+2], NULL);
+                }
+                while(finishedpid < numStudents){
+                    //printf("%d", finishedpid);
+                    int status = 0;
+                    //printf("%s%ld", "\n", );
+                    int finished = waitpid(pids[j], &status, WNOHANG);
+                    if(WIFEXITED(status)){
+                        int ret = WEXITSTATUS(status);
+                        if(ret == 0){
+                            printf("%s", "correct \n");
+                            finishedpid++;
+                            count++;
+                            index++;
+                            //printf("%d%s", ret, "\n");
+                        }
+                        else if(ret == 1){
+                            printf("%s", "incorrect\n");
+                            finishedpid++;
+                            index++;
+                        }
+                        else if(ret == 3){
+                            printf("%s", "seg fault\n");
+                            finishedpid++;
+                            index++;
+                        }
+                        else{
+                            break;
+                        }
+                    }
                     
-            } 
-            else if(pid < 0){
-                perror("failed");
-                exit(EXIT_FAILURE);
-                    
+                }
             }
-        }
-        
-    }
-    printf("%sc\n", students);
+            finishedpid = 0;
+            if(i<3){
+                memset(pids, 0, sizeof(pids)); 
+                printf("%s%d%s","\nnew\n", pids[1], "\n");
 
-    while(1){
-        int status = 0;
-        int finished = waitpid(-1, &status, WNOHANG);
-        if(finished > 0){
-            if(WIFEXITED(status)){
-                int ret = WEXITSTATUS(status);
-                if(ret == 0){
-                    printf("%s", "correct");
-                }
-                else if(ret == 1){
-                    printf("%s", "incorrect");
-                }
-                else if(ret == 3){
-                    printf("%s", "seg fault");
-                }
+                break;
             }
+
         }
-        else if(finished == 0){
-            break;
-        }
-        else if(finished == -1){
-            perror('waitpid() failed');
-            exit(EXIT_FAILURE)
-        }
-    }
+    
+
     fclose(file);
     return 0;
 }  
