0b2c2203389cc9c7ec1190a058a5cadfeb2f8f73
yeamelack <150837167+yeamelack@users.noreply.github.com>
2024-02-09 13:03:38 -0600

    Update WIPautograder.c

--- WIPautograder.c
+++ WIPautograder.c
@@ -13,62 +13,62 @@ int main(int argc, char *argv[]) {
     }
 
     pid_t pids[argc];
-    struct timeval timer[argc];
-    char buff[128];
+   
     int numStudents = atoi(argv[1]);
     char students[numStudents][WORD_LEN];
     int finishedpid = 0;
-    int count = 0;
-    int index = 0;
+
     
     
 
-    for(int i=0; i < 3; i++){
-        for(int j=0; j<atoi(argv[1]); j++){
+    for(int i=0; i < 1; i++){
+        for(int j=0; j < 1; j++){
             pid_t pid = fork();
             if(pid>0){
                     pid_t terminated_pid = wait(NULL);
                 }
                 else if(pid == 0){
                     pids[j] = getpid();
-                    printf("%s%d", "\n", pids[j]);
+                    printf("%s%d%s", "\n", pids[j], "\n");
                     execl(students[i], argv[1], argv[j+2], NULL);
                 }
                 while(finishedpid < numStudents){
-                    //printf("%d", finishedpid);
+                
+             
                     int status = 0;
-                    //printf("%s%ld", "\n", );
                     int finished = waitpid(pids[j], &status, WNOHANG);
-                    if(WIFEXITED(status)){
-                        int ret = WEXITSTATUS(status);
-                        if(ret == 0){
-                            printf("%s", "correct \n");
-                            finishedpid++;
-                            count++;
-                            index++;
-                            //printf("%d%s", ret, "\n");
+
+                    if(finished > 0){
+                        if(WIFEXITED(status)){
+                            int ret = WEXITSTATUS(status);
+                            if(ret == 0){
+                                printf("%s", "correct \n");
+                                
+                                finishedpid++;
+                            
+                            
+                            }
+                            else if(ret == 1){
+                                printf("%s", "incorrect\n");
+                                finishedpid++;
+                            
+                            }
+                            else if(ret == 3){
+                                printf("%s", "seg fault\n");
+                                finishedpid++;
+                        
+                            }
+                            else{
+                                break;
+                            }
                         }
-                        else if(ret == 1){
-                            printf("%s", "incorrect\n");
-                            finishedpid++;
-                            index++;
-                        }
-                        else if(ret == 3){
-                            printf("%s", "seg fault\n");
-                            finishedpid++;
-                            index++;
-                        }
-                        else{
-                            break;
                         }
                     }
-                    
                 }
-            }
             finishedpid = 0;
             if(i<3){
-                memset(pids, 0, sizeof(pids)); 
-                printf("%s%d%s","\nnew\n", pids[1], "\n");
+                //memset(pids, 0, sizeof(pids)); 
+                
 
                 break;
             }
