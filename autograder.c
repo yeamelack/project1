86f046b30d0ac986720845c1c6f698002a2c76ad
Lev632 <114541520+Lev632@users.noreply.github.com>
2024-02-10 12:44:38 -0600

    Add files via upload

--- autograder.c
+++ autograder.c
@@ -21,6 +21,10 @@ int main(int argc, char *argv[]) {
     char students[numStudents][WORD_LEN];
     int count = 0;
     int inc = 0;
+    pid_t pids[numStudents];
+    int finishedpid = 0;
+    int batch = 1;
+    int i, j;
     
     while(fgets(buff, sizeof(buff), file) != NULL){
         buff[strcspn(buff, "\n")] = 0;
@@ -28,12 +32,6 @@ int main(int argc, char *argv[]) {
         k++;
     }
     fclose(file);
-    
-    pid_t pids[numStudents];
-    int finishedpid = 0;
-    int batch = 1;
-    int i, j;
-
     for(i=2; i < argc; i++){//running through arguments 
         int finishedpid = 0;  
         pid_t pid;
@@ -55,55 +53,61 @@ int main(int argc, char *argv[]) {
             else {
                 pids[inc] = pid;
                 printf("%s %d %d\n", "child ", inc, pids[inc]);
-                // sleep(1);
                 inc++;
             }
             
         }
         inc = 0;
         while(finishedpid < numStudents){
-            int status = 0;
-            int pidStatus = waitpid(-1, &status, WNOHANG);
-            printf("\npid status %d\n", pidStatus);
-            if (pidStatus == 0){
-                printf("child processes still running\n");
-            }
-            if(pidStatus == -1){
-                printf("\n%d\n", 9090);
-                pids[0] = 0;
-                finishedpid++;             
-            }
-
-            if(pidStatus > 0){
-                printf("child %d finished\n", pidStatus);
-                if(WIFEXITED(status) > 0){
-                    int ret = WEXITSTATUS(status);
-                    printf("return status %d\n", ret);
-                    if(ret == 0){
-                        printf("%s", "correct \n");
-                        pids[0] = 0;
-                        finishedpid++;
-                        
+            for (int i = 0; i < numStudents; i++){
+                int status = 0;
+                int pidStatus = waitpid(pids[i], &status, WNOHANG);
+                printf("\npid status %d\n", pidStatus);
+                if (pidStatus == 0){
+                    printf("child processes still running\n");
+                }
+                if(pidStatus == -1){
+                    printf("error in process\n");
+                    pids[0] = 0;
+                    finishedpid++;             
+                }
+                // if pidstatus is greater than 0 it means child terminated 
+                if(pidStatus > 0){
+                    printf("child %d finished\n", pidStatus);
+                    // if what WIFEXITED returns is > 0 it means the child succesfully terminated
+                    if(WIFEXITED(status) > 0){
+                        //checking the return status of the succesfully terminated child
+                        int ret = WEXITSTATUS(status);
+                        printf("return status %d\n", ret);
+                        if(ret == 0){
+                            printf("%s", "correct \n");
+                            pids[0] = 0;
+                            finishedpid++;
+                            
+                        }
+                        else if(ret == 1){
+                            printf("%s", "incorrect\n");
+                            finishedpid++; 
+                            pids[0] = 0;
+                            
+                        }
                     }
-                    else if(ret == 1){
-                        printf("%s", "incorrect\n");
-                        finishedpid++; 
-                        pids[0] = 0;
-                        
+                    // otherwise WIFEXITED returns 0 child terminated but not normally
+                    else {
+                        int ret = WEXITSTATUS(status);
+                        printf("return status %d\n", ret);
+                        // because child did not terminate normally  WEXITSTATUS is 0 means incorrect 
+                        if (ret == 0){
+                            printf("%s", "incorrect seg fault\n");
+                            finishedpid++; 
+                            pids[0] = 0;
+                        }
                     }
                 }
-                else {
-                    int ret = WEXITSTATUS(status);
-                    printf("return status %d\n", ret);
-                    if (ret == 0){
-                        printf("%s", "incorrect seg fault\n");
-                        finishedpid++; 
-                        pids[0] = 0;
-                    }
+                printf("dead kids: %d num of kids %d\n\n", finishedpid, numStudents);
+                sleep(1);
                 }
-            }
-            printf("dead kids: %d num of kids %d\n\n", finishedpid, numStudents);
-            sleep(1);
+            
         }
         batch++;
         inc = 0;    
