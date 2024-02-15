3734aca4f60050bd304ae5a53bb6b80802d387ec
Lev632 <114541520+Lev632@users.noreply.github.com>
2024-02-15 14:42:41 -0600

    Add files via upload

--- autograder.c
+++ autograder.c
@@ -35,7 +35,7 @@ int casecheck(pid_t pid) {
 
 int main(int argc, char *argv[]) {
 
-    write_filepath_to_submissions("test_solutions2", "submission.txt");
+    write_filepath_to_submissions("test_solutions", "submission.txt");
     FILE *file = fopen("submission.txt", "r");
       if(!file){
         return -1;
@@ -51,15 +51,14 @@ int main(int argc, char *argv[]) {
 
     // char scores[total];
     char students[total_executables][WORD_LEN];
-   
     int batch_size = atoi(argv[1]);
-  
     int iter = 0;
     while(fgets(buff, sizeof(buff), file) != NULL){
         buff[strcspn(buff, "\n")] = 0;
         strcpy(students[iter], buff);
         iter++;
     }
+
     for(int i =0 ;i<total_executables; i++){
     printf("array stu %s\n", students[i]);
     }
@@ -96,83 +95,84 @@ int main(int argc, char *argv[]) {
                     }       
                 count++;
                 //printf("count is %d\n", count);
-        
             }
-
-            // for(int i=0; i<2; i++){
-            //         printf("pids array at %d is %d:\n", i, pids[i]);
-            //     }
-            
 
             int finished = 0;
             while(finished < batch_size){
                 sleep(1);
-                // for(int i=0; i<2; i++){
-                //     printf("pids array at %d is %d:\n", i, pids[i]);
-                // }
                 for(int k=0; k < batch_size; k++){
                     int status = 0;
                    // printf("child getting checked %d pids at %d\n",k, pids[k]);
-
                     int pidStatus = waitpid(pids[k], &status, WNOHANG);
                     timer[k] = stop_timer(&start);
-                    printf("\ntimer: %d\n\n", timer[k]);
+                    // printf("\ntimer: %d\n\n", timer[k]);
                     //printf("pidstat getting checked %d\n", pidStatus);
-                        if(pidStatus == -1){
-                            printf("error in process\n");
-                            pids[0] = 0;
-           
+                    if(pidStatus == -1){
+                        printf("error in process\n");
+                        pids[0] = 0;
+        
+                    }
+                    // if pidstatus is greater than 0 it means child terminated 
+                    if (pidStatus > 0){//BRANCH GOOD
+                    // if what WIFEXITED returns is > 0 it means the child succesfully terminated
+                        if(WIFEXITED(status) > 0){ 
+                            //checking the return status of the succesfully terminated child
+                            int ret = WEXITSTATUS(status);
+                            if(ret == 0){ // CORRECT
+                                printf("pid: %d %s", pids[k], "correct \n");
+                                sleep(1);
+                                pids[k] = 0;
+                                finished++;
+                            }
+                            else if(ret == 1){ //INCORRECT
+                                printf("pid: %d %s", pids[k] , "incorrect\n");
+                                sleep(1);
+                                finished++; 
+                                pids[k] = 0;
+                            }
+                            
                         }
-
+                        else if (WIFSIGNALED(status) > 0) { //SEGFAULT GOOD
+                                printf("Pid %d incorrect seg fault\n", pids[k]);
+                                finished++; 
+                                pids[k] = 0;
+                        }
+                    }
                     else if (pidStatus == 0 && casecheck(pids[k]) == 0){ //STUCK BLOCKED NEEDS CHECK
-                        printf("child %d finished\n", pidStatus);
-                        printf("Stuck/blocked\n\n");
-                        finished++;
+                        sleep(10);
+                        int status2 = 0;
+                        printf("pids %d\n",pids[k]);
+                        int pidStatus2 = waitpid(pids[k], &status2, WNOHANG);
+                        printf("pidstat2 getting checked %d\n", pidStatus2);
+                        if (pidStatus2 > 0){
+                            printf("slow\n");
+                            finished++;
+
+                        }
+                        else {
+                            printf("child %d finished\n", pidStatus);
+                            printf("Pid %d Stuck/blocked\n", pids[k]);
+                            finished++;
+                        }
+                        
                     }
                     else if (pidStatus == 0 && casecheck(pids[k]) == 1){//INFINITE NEEDS CHECK
                         // printf("get pid %d\n", getpid());
                         printf("child %d finished\n", pidStatus);
-                        printf("Infinite loop\n");
+                        printf("Pid %d Infinite loop\n", pids[k]);
                         kill(pids[k], SIGKILL);
                         finished++;
-                }
-                if (pidStatus == 0){
-                            //printf("child processes still running\n");
-                            sleep(1);
-                        }
-
-                         // if pidstatus is greater than 0 it means child terminated 
-                if (pidStatus > 0){//BRANCH GOOD
-                    // if what WIFEXITED returns is > 0 it means the child succesfully terminated
-                    if(WIFEXITED(status) > 0){ 
-                        //checking the return status of the succesfully terminated child
-                        int ret = WEXITSTATUS(status);
-                        if(ret == 0){ // CORRECT
-                            printf("pid: %d %s", pids[k], "correct \n");
-                            sleep(1);
-                            pids[k] = 0;
-                            finished++;
-                        }
-                        else if(ret == 1){ //INCORRECT
-                            printf("pid: %d %s", pids[k] , "incorrect\n");
-                            sleep(1);
-                            finished++; 
-                            pids[k] = 0;
-                        }
-                        
                     }
-                    else if (WIFSIGNALED(status) > 0) { //SEGFAULT GOOD
-                            printf("%s", "incorrect seg fault\n");
-                            finished++; 
-                            pids[k] = 0;
+                    if (pidStatus == 0){
+                        //printf("child processes still running\n");
+                        sleep(1);
                     }
                 }
+                sleep(1);
+                // printf("done exe %d\n", done_executables);
+                // printf("total exe %d\n", total_executables);
             }
-            sleep(1);
-                        // printf("done exe %d\n", done_executables);
-            // printf("total exe %d\n", total_executables);
-         }
-         done_executables += batch_size;
+            done_executables += batch_size;
         }
     }
     fclose(file);
