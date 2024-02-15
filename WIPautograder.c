c6e21233433362e4e7a95fb3592105257f8e68ec
yeamelack <150837167+yeamelack@users.noreply.github.com>
2024-02-14 20:05:47 -0600

    Update WIPautograder.c

--- WIPautograder.c
+++ WIPautograder.c
@@ -8,110 +8,80 @@
 
 int main(int argc, char *argv[]) {
 
-    write_filepath_to_submissions("/home/erist003/4061/p1/test", "/home/erist003/4061/p1/submission.txt");
+    write_filepath_to_submissions("test_solutions", "submission.txt");
     FILE *file = fopen("submission.txt", "r");
       if(!file){
         return -1;
     }
 
-    int k = 0;
-    struct timeval timer[argc];
+    int total_executables = 0;
     char buff[128];
-    int numStudents = atoi(argv[1]);
-    char students[numStudents][WORD_LEN];
-    int count = 0;
-    int inc = 0;
-    
+    while(fgets(buff, sizeof(buff), file) != NULL){
+        total_executables++;
+    }
+    rewind(file);
+    printf("student array: %d\n", total_executables);
+
+    // char scores[total];
+    char students[total_executables][WORD_LEN];
+   
+    int batch_size = atoi(argv[1]);
+  
+    int iter = 0;
     while(fgets(buff, sizeof(buff), file) != NULL){
         buff[strcspn(buff, "\n")] = 0;
-        strcpy(students[k], buff);
-        k++;
+        strcpy(students[iter], buff);
+        iter++;
+    }
+
+    pid_t pids[batch_size];
+
+    
+    for(int i = 2; i < argc; i++){
+        int done_executables = 0;
+        while (done_executables < total_executables){
+            for(int j = 0; j < batch_size; j++){
+                pid_t pid = fork();
+                    if (pid < 0) {
+                        perror("fork failed");
+                        return -1;
+                    }
+                    else if(pid == 0){
+                        // printf("student: %s, answer%s, pid: %d\n", students[j], argv[i], getpid());
+                        printf("get pid %d\n", getpid());
+                        //start_timer(&start);
+                        execl(students[done_executables], argv[1], argv[i], NULL);
+                        perror("exec failed\n");
+                        exit(1);
+                    }
+                    else {
+                        pids[j] = pid;
+                        printf("%s %d %d\n", "child ", j, pids[j]);
+                    }      
+                    done_executables++;   
+
+
+            }
+             printf("\n");
+
+        //     int finished = 0;
+        //     while(finished < batch_size){
+        //         for(int k=0; k<batch_size; k++){
+
+
+        //         }
+
+        //     }
+        //     done_executables += batch_size;
+         }
+
+
+
+
+
+
+
     }
     fclose(file);
+}
 
-    pid_t pids[numStudents];
-    int finishedpid = 0;
-    int i, j;
-    for(i=2; i < argc; i++){//running through arguments 
-        int finishedpid = 0;  
-        for(j=0 ; j < numStudents; j++){    
-            int pid = fork();
-            if(pid > 0){
-                pids[inc] = pid;
-                inc++;
-                printf("%s%d %d\n", "in if inc ", inc, pids[inc]);
-                }
-                else if(pid == 0){
-                    //printf("%d%s", pids[j],"\n\n");
-                    execl(students[j], argv[1], argv[i], NULL);
-                    perror("exec failed");
-                    exit(1);
-                }
-                else {
-                    fprintf(stderr, "Fork failed\n");
-                    return 1;
-                }
-                //printf("\n%d\n", finishedpid);
-                //printf("%d\n", finishedpid);
-            }
-            //printf("%d%s", pids[0],"\n\n");
-            inc = 0;
-            while(finishedpid <= numStudents){
-                    printf("%s%d%s","in loop ", pids[inc],"\n");
-
-                    pids[inc] = getpid();
-                    //printf("%d%s",pids[j] ,"\n\n");
-                    
-                    int status = 0;
-                    //printf("%s%d", "\nhi", pids[j]);
-                   // printf("%d\n", pids[j]);
-                    int finished = waitpid(pids[0], &status, WNOHANG);
-                    inc++;
-                    if(finished == -1){
-
-                        printf("\n%d\n", 9090);
-                        pids[0] = 0;
-                        finishedpid++;
-                        break;
-                    }
-
-                    if(finished > 0){
-                        if(WIFEXITED(status)){
-                            int ret = WEXITSTATUS(status);
-                            if(ret == 0){
-                            
-                                printf("%s", "correct \n");
-                                //printf("%s\n", argv[i]);
-                                pids[0] = 0;
-                                finishedpid++;
-                                break;
-                            }
-                            else if(ret == 1){
-                                printf("%s", "incorrect\n");
-                                finishedpid++; 
-                                pids[0] = 0;
-                                //printf("%s\n", argv[i]);
-                                 
-                            }
-                            else if(ret == 3){
-                                printf("%s", "seg fault\n");
-                                finishedpid++;
-                                pids[0] = 0;
-                                //printf("%s\n", argv[i]);
-                            }
-                            else if(ret == 6){
-                                printf("%s", "seg fault\n");
-                                finishedpid++;
-                                pids[0] = 0;
-                                //printf("%s\n", argv[i]);
-                            }
-                        }
-                    }
-        
-                }
-                inc = 0;
-        }
-        
-        return 0;
-    }
-            
